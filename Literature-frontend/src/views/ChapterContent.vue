4ff16e18bbcf393a7b0578963883de14274fc963
czl0325 <>
2021-04-13 18:02:07 +0800

    准备做小说内容

---
+++ Literature-frontend/src/views/ChapterContent.vue
@@ -0,0 +1,28 @@
+<template>
+  <navigation-bar title="章节详情"/>
+  <div class="my-container">
+    <p>{{ chapter.chapter_name }}</p>
+    <div v-html="chapter.content"></div>
+  </div>
+</template>
+
+<script lang="ts">
+import {defineComponent, ref} from 'vue'
+import NavigationBar from "@/components/NavigationBar.vue";
+import {ChapterModel} from "@/models/models";
+
+export default defineComponent({
+  name: "ChapterContent",
+  components: {NavigationBar},
+  setup() {
+    const chapter = ref<ChapterModel>({})
+    return {
+      chapter
+    }
+  }
+})
+</script>
+
+<style scoped>
+
+</style>
